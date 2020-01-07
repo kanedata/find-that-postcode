37678abe1c7ecee21507e6afb8c2069750ae4d8e
David Kane <david@dkane.net>
2020-01-07 12:38:59 +0000

    add command to initialise elasticsearch mappings

--- findthatpostcode/db.py
+++ findthatpostcode/db.py
@@ -4,6 +4,23 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+INDEXES = {
+    "geo_postcode": {
+        "properties": {
+            "location": {"type": "geo_point"}
+        }
+    },
+    "geo_placename": {
+        "properties": {
+            "location": {"type": "geo_point"}
+        }
+    },
+    "geo_area": {
+        "properties": {
+            "boundary": {"type": "geo_shape"}
+        }
+    }
+}
 
 def get_db():
     if 'db' not in g:
@@ -15,9 +32,20 @@ def get_db():
 def close_db(e=None):
     db = g.pop('db', None)
 
-def init_db():
-    db = get_db()
-    # @TODO: create elasticsearch index mappings here
+def init_db(reset=False):
+    es = get_db()
+    doc_type = '_doc'
+
+    for index, mapping in INDEXES.items():
+        if es.indices.exists(index) and reset:
+            click.echo("[elasticsearch] deleting '%s' index..." % (index))
+            res = es.indices.delete(index=index)
+            click.echo("[elasticsearch] response: '%s'" % (res))
+        click.echo("[elasticsearch] creating '%s' index..." % (index))
+        res = es.indices.create(index=index)
+
+        res = es.indices.put_mapping(doc_type=doc_type, body=mapping, index=index, include_type_name=True)
+        click.echo("[elasticsearch] set mapping on %s index, %s type" % (index, doc_type))
 
 
 def init_app(app):
@@ -26,8 +54,9 @@ def init_app(app):
 
 
 @click.command('init-db')
+@click.option('--reset/--no-reset', default=False)
 @with_appcontext
-def init_db_command():
+def init_db_command(reset):
     """Clear the existing data and create new tables."""
-    init_db()
+    init_db(reset)
     click.echo('Initialized the database.')

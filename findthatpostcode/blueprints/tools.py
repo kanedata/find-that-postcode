44064218af51c515f37293cc4ac55a0a92d6aa70
David Kane <david@dkane.net>
2022-12-06 18:26:52 +0000

    add reduce geojson filesize tool

--- findthatpostcode/blueprints/tools.py
+++ findthatpostcode/blueprints/tools.py
@@ -6,3 +6,8 @@ bp = Blueprint("tools", __name__, url_prefix="/tools")
 @bp.route("/merge-geojson", strict_slashes=False, methods=["GET"])
 def merge_geojson():
     return render_template("merge-geojson.html.j2")
+
+
+@bp.route("/reduce-geojson", strict_slashes=False, methods=["GET"])
+def reduce_geojson():
+    return render_template("reduce-geojson.html.j2")
